c4462a56591691a490ddf12f23682a42d7314ac3
shaggyze <shaggyze@gmail.com>
2024-11-10 16:35:23 -0800

    update index.php

--- index.php
+++ index.php
@@ -3,7 +3,12 @@
 // Return json file
 header('Content-type:application/json;charset=utf-8');
 
-// ini_set('max_execution_time', 0);
+ini_set('max_execution_time', 2000);
+ini_set('memory_limit', "1024M");
+ini_set('max_file_size', 1000000000);
+
+// Set the default timezone to UTC
+date_default_timezone_set('UTC');
 
 // Use the Mal-Scraper library
 require 'vendor/autoload.php';
@@ -13,7 +18,7 @@ date_default_timezone_set('UTC');
 
 $myMalScraper = new MalScraper([
     'enable_cache' => true,
-    'cache_time'   => 3600,
+    'cache_time'   => 21600,
     'to_api'       => true,
 ]);
 
@@ -30,11 +35,25 @@ $page = isset($_GET['p']) ? $_GET['p'] : 1;
 
 $query = isset($_GET['q']) ? $_GET['q'] : '';
 
+$genre = isset($_GET['g']) ? $_GET['g'] : 0;
+
 $year = isset($_GET['y']) ? $_GET['y'] : date('Y');
 $season = isset($_GET['s']) ? $_GET['s'] : getCurrentSeason();
 
 $user = isset($_GET['u']) ? $_GET['u'] : '';
 $status = isset($_GET['st']) ? $_GET['st'] : 7;
+
+$size = isset($_GET['sz']) ? $_GET['sz'] : '';
+
+function get_subdirectory($type, $id) {
+  $id = intval($id);
+  $subdirectory_number = floor($id / 10000);
+  $subdirectory_path = '../info/' . $type . '/' . $subdirectory_number . '/';
+  if (!is_dir($subdirectory_path) && ($type == "anime" || $type == "manga")) {
+    mkdir($subdirectory_path, 0777, true); // Create directory with permissions
+  }
+  return strval($subdirectory_number);
+}
 
 // Call the requested method
 switch ($method) {
@@ -43,9 +62,22 @@ switch ($method) {
     case 'info':
         if ($type && $id) {
             $result = $myMalScraper->getInfo($type, $id);
-            print_r($result);
+			$subdirectory = get_subdirectory($type, $id);
+			$decodedResult = json_decode($result, true);
+			$filePath = '../info/' . $type . '/' . $subdirectory . '/' . $id . '.json';
+			if ($decodedResult['status'] == 200) {
+				file_put_contents($filePath, $result, FILE_USE_INCLUDE_PATH);
+				print_r($result);
+			} else {
+				if (file_exists($filePath)) {
+					$file = file_get_contents($filePath, FILE_USE_INCLUDE_PATH);
+					print_r($file);
+				} else {
+					print_r($result);
+				}
+			}
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
     case 'character':
@@ -53,7 +85,7 @@ switch ($method) {
             $result = $myMalScraper->getCharacter($id);
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
     case 'people':
@@ -61,7 +93,7 @@ switch ($method) {
             $result = $myMalScraper->getPeople($id);
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
     case 'studio-producer':
@@ -70,7 +102,7 @@ switch ($method) {
             $result = $myMalScraper->getStudioProducer($id, $page);
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
     case 'magazine':
@@ -78,7 +110,7 @@ switch ($method) {
             $result = $myMalScraper->getMagazine($id, $page);
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
     case 'genre':
@@ -86,7 +118,7 @@ switch ($method) {
             $result = $myMalScraper->getGenre($type, $id, $page);
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
     case 'review':
@@ -94,7 +126,7 @@ switch ($method) {
             $result = $myMalScraper->getReview($id);
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
     case 'recommendation':
@@ -102,7 +134,7 @@ switch ($method) {
             $result = $myMalScraper->getRecommendation($type, $id1, $id2);
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
 
@@ -113,7 +145,7 @@ switch ($method) {
             $result = $myMalScraper->getCharacterStaff($type, $id);
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
     case 'stat':
@@ -121,33 +153,46 @@ switch ($method) {
             $result = $myMalScraper->getStat($type, $id);
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
     case 'picture':
         if ($type && $id) {
             $result = $myMalScraper->getPicture($type, $id);
+			if (strtolower($size) == "small") {
+				$result = str_replace('l.jpg', 't.jpg', $result);
+			} elseif (strtolower($size) == "medium") {
+				$result = str_replace('l.jpg', '.jpg', $result);
+			}
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
     case 'character-picture':
     case 'characterpicture':
         if ($id) {
             $result = $myMalScraper->getCharacterPicture($id);
+			if (strtolower($size) == "small") {
+				$result = str_replace('l.jpg', 't.jpg', $result);
+			} elseif (strtolower($size) == "medium") {
+				$result = str_replace('l.jpg', '.jpg', $result);
+			}
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
     case 'people-picture':
     case 'peoplepicture':
         if ($id) {
             $result = $myMalScraper->getPeoplePicture($id);
+			if ($size == "medium") {
+				$result = str_replace('l.jpg', '.jpg', $result);
+			}
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
     case 'video':
@@ -155,7 +200,7 @@ switch ($method) {
             $result = $myMalScraper->getVideo($id, $page);
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
     case 'episode':
@@ -163,7 +208,7 @@ switch ($method) {
             $result = $myMalScraper->getEpisode($id, $page);
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
     case 'anime-review':
@@ -172,7 +217,7 @@ switch ($method) {
             $result = $myMalScraper->getAnimeReview($id, $page);
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
     case 'manga-review':
@@ -181,7 +226,7 @@ switch ($method) {
             $result = $myMalScraper->getMangaReview($id, $page);
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
     case 'anime-recommendation':
@@ -190,7 +235,7 @@ switch ($method) {
             $result = $myMalScraper->getAnimeRecommendation($id);
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
     case 'manga-recommendation':
@@ -199,7 +244,7 @@ switch ($method) {
             $result = $myMalScraper->getMangaRecommendation($id);
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
 
@@ -242,7 +287,7 @@ switch ($method) {
             $result = $myMalScraper->searchAnime($query, $page);
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
     case 'search-manga':
@@ -251,7 +296,7 @@ switch ($method) {
             $result = $myMalScraper->searchManga($query, $page);
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
     case 'search-character':
@@ -260,7 +305,7 @@ switch ($method) {
             $result = $myMalScraper->searchCharacter($query, $page);
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
     case 'search-people':
@@ -269,7 +314,7 @@ switch ($method) {
             $result = $myMalScraper->searchPeople($query, $page);
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
     case 'search-user':
@@ -278,7 +323,7 @@ switch ($method) {
             $result = $myMalScraper->searchUser($query, $page);
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
 
@@ -318,7 +363,7 @@ switch ($method) {
             $result = $myMalScraper->getUser($user);
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
     case 'user-friend':
@@ -327,7 +372,7 @@ switch ($method) {
             $result = $myMalScraper->getUserFriend($user);
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
     case 'user-history':
@@ -336,17 +381,81 @@ switch ($method) {
             $result = $myMalScraper->getUserHistory($user, $type);
             print_r($result);
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
         }
         break;
     case 'user-list':
     case 'userlist':
         if ($user) {
             $type = $type ? $type : 'anime';
-            $result = $myMalScraper->getUserList($user, $type, $status);
-            print_r($result);
+            $genre = $genre ? $genre : 0;
+            $result = $myMalScraper->getUserList($user, $type, $status, $genre);
+			$decodedResult = json_decode($result, true);
+			$filePath = '../userlist/' . $user . '_' . $type . '_' . $status . '_' . $genre . '.json';
+			if ($decodedResult['status'] == 200) {
+				/*$timestamp = date('Y-m-d\TH:i:s.u\Z');
+				$result = "/* Accessed  " . $timestamp . " *[DEL]/ \r" . $result;*/
+				
+				file_put_contents($filePath, $result, FILE_USE_INCLUDE_PATH);
+
+				print_r($result);
+			} else {
+
+				if (file_exists($filePath)) {
+					$file = file_get_contents($filePath, FILE_USE_INCLUDE_PATH);
+					print_r($file);
+				} else {
+					print_r($result);
+				}
+			}
         } else {
-            print_r(paramError());
+            print_r(paramError('2'));
+        }
+        break;
+	case 'user-css':
+	case 'usercss':
+        if ($user) {
+            header('Content-Type: text/css');
+
+            $type = $type ? $type : 'anime';
+            $genre = $genre ? $genre : 0;
+            $query = $query ? $query : false;
+
+            $result = $myMalScraper->getUserCSS($user, $type, $query, $genre);
+			$decodedResult = json_decode($result, true);
+			$result = json_decode($result, true);
+			if ($result['data'] == 'No UserList') {
+				$result = $result['data'];
+				print_r(paramError('3'));
+
+				break;
+			}
+            $result = $result['data'];
+			$result = str_replace('-a', '\a', $result);
+			if (strtolower($size) == "small") {
+				$result = str_replace('l.jpg', 't.jpg', $result);
+			} elseif (strtolower($size) == "medium") {
+				$result = str_replace('l.jpg', '.jpg', $result);
+			}
+			$filePath = '../cache/' . $user . '_' . $type . '_' . hash('sha256', (urlencode($query))) . '_' . $genre . '.css';
+			if ($decodedResult['status'] == 200) {
+				$timestamp = date('Y-m-d\TH:i:s.u\Z');
+				$result = "/* Accessed  " . $timestamp . " */ \r" . $result;
+
+				file_put_contents($filePath, $result, FILE_USE_INCLUDE_PATH);
+
+				print_r($result);
+			} else {
+
+				if (file_exists($filePath)) {
+					$file = file_get_contents($filePath, FILE_USE_INCLUDE_PATH);
+					print_r($file);
+				} else {
+					print_r($result);
+				}
+			}
+		} else {
+            print_r(paramError('2'));
         }
         break;
     case 'user-cover':
@@ -355,20 +464,49 @@ switch ($method) {
             header('Content-Type: text/css');
 
             $type = $type ? $type : 'anime';
+            $genre = $genre ? $genre : 0;
             $query = $query ? $query : false;
 
-            $result = $myMalScraper->getUserCover($user, $type, $query);
-            $result = json_decode($result, true);
-            $result = $result['data'];
+            $result = $myMalScraper->getUserCover($user, $type, $query, $genre);
+			$decodedResult = json_decode($result, true);
+			$result = json_decode($result, true);
+			if ($result['data'] == 'No UserList') {
+				$result = $result['data'];
+				print_r(paramError('3'));
 
-            print_r($result);
-        } else {
-            print_r(paramError());
+				break;
+			}
+            $result = $result['data'];
+			if (strtolower($size) == "small") {
+				$result = str_replace('l.jpg', 't.jpg', $result);
+			} elseif (strtolower($size) == "medium") {
+				$result = str_replace('l.jpg', '.jpg', $result);
+			}
+			$filePath = '../cache/' . $user . '_' . $type . '_' . hash('sha256', (urlencode($query))) . '_' . $genre . '.css';
+			if ($decodedResult['status'] == 200) {
+				$timestamp = date('Y-m-d\TH:i:s.u\Z');
+				$result = "/* Accessed  " . $timestamp . " */ \r" . $result;
+				
+				file_put_contents('../cache/' . $user . '_' . $type . '_' . hash('sha256', (urlencode($query))) . '_' . $genre . '.css', $result, FILE_USE_INCLUDE_PATH);
+
+				print_r($result);
+			} else {
+
+				if (file_exists($filePath)) {
+					$file = file_get_contents($filePath, FILE_USE_INCLUDE_PATH);
+					print_r($file);
+				} else {
+					print_r($result);
+				}
+			}
+		} else {
+            print_r(paramError('2'));
         }
         break;
-
+		
 // Secret ----------
     case 'auto-cover':
+    case 'autocover':
         header('Content-Type: text/css');
 
         $user_url = $_SERVER['HTTP_REFERER'];
@@ -382,34 +520,40 @@ switch ($method) {
 
         $user = $user_url;
         $type = $type ? $type : 'anime';
+        $genre = $genre ? $genre : 0;
         $query = $query ? $query : false;
 
-        $result = $myMalScraper->getUserCover($user, $type, $query);
+        $result = $myMalScraper->getUserCover($user, $type, $query, $genre);
         $result = json_decode($result, true);
         $result = $result['data'];
 
         print_r($result);
         break;
     default:
-        print_r(paramError(true));
+        print_r(paramError('1'));
         break;
 }
 
 // Return error parameter
-function paramError($a = false)
+function paramError($a = '2')
 {
-    $result = [];
-    if ($a) {
-        header('HTTP/1.1 404');
-        $result['status'] = 404;
-        $result['status_message'] = 'Method not found';
-        $result['data'] = [];
-    } else {
+	$result = [];
+    if ($a == '1') {
         header('HTTP/1.1 400');
         $result['status'] = 400;
-        $result['status_message'] = 'Bad Request';
+        $result['message'] = 'Method not found';
         $result['data'] = [];
-    }
+    } elseif ($a == '2') {
+        header('HTTP/1.1 400');
+        $result['status'] = 400;
+        $result['message'] = 'Bad Request';
+        $result['data'] = [];
+    } else {
+		header('HTTP/1.1 404');
+		$result['status'] = 404;
+		$result['message'] = 'No UserList';
+        $result['data'] = [];
+	}
 
     return json_encode($result, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
 }
